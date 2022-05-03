409298a4e02e2b1e2c9686faf5faca726c809828
Moein Latifi <moeinl1090@protonmail.com>
2022-05-03 09:19:36 +0430

    designed fragment news

---
+++ app/src/main/java/ir/ariyana/news_application_mvvm/ui/fragments/FragmentNews.kt
@@ -0,0 +1,26 @@
+package ir.ariyana.news_application_mvvm.ui.fragments
+
+import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import androidx.fragment.app.Fragment
+import ir.ariyana.news_application_mvvm.databinding.FragmentBreakingNewsBinding
+
+class FragmentNews : Fragment() {
+
+    private lateinit var binding : FragmentBreakingNewsBinding
+
+    override fun onCreateView(
+        inflater: LayoutInflater,
+        container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        binding = FragmentBreakingNewsBinding.inflate(layoutInflater, container, false)
+        return binding.root
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+    }
+}
