fa61e986bb9ad23a9344e72ba46c5e04e47b6e4c
Moein Latifi <moeinl1090@protonmail.com>
2022-05-03 12:23:13 +0430

    added service api .kt file

---
+++ app/src/main/java/ir/ariyana/news_application_mvvm/api/ServiceAPI.kt
@@ -0,0 +1,23 @@
+package ir.ariyana.news_application_mvvm.api
+
+import ir.ariyana.news_application_mvvm.model.NewDataClass
+import ir.ariyana.news_application_mvvm.utils.Constants
+import retrofit2.Response
+import retrofit2.http.GET
+import retrofit2.http.Query
+
+interface ServiceAPI {
+
+    @GET("top-headlines")
+    suspend fun getBreakingNews(
+        @Query("country") countryCode : String = "us",
+        @Query("page") page : Int = 1,
+        @Query("apiKey") apiKey : String = Constants.API_KEY
+    ) : Response<NewDataClass>
+
+    @GET("everything")
+    suspend fun getSearchedNews(
+        @Query("q")
+        query : String
+    ) : Response<NewDataClass>
+}
