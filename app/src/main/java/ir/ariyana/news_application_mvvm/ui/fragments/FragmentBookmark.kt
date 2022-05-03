1f91cc31dab4dd276021d1f6d8e9b20f47e707d6
Moein Latifi <moeinl1090@protonmail.com>
2022-05-03 09:33:19 +0430

    designed fragment bookmark

---
+++ app/src/main/java/ir/ariyana/news_application_mvvm/ui/fragments/FragmentBookmark.kt
@@ -0,0 +1,26 @@
+package ir.ariyana.news_application_mvvm.ui.fragments
+
+import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import androidx.fragment.app.Fragment
+import ir.ariyana.news_application_mvvm.databinding.FragmentBookmarkBinding
+
+class FragmentBookmark : Fragment() {
+
+    private lateinit var binding : FragmentBookmarkBinding
+
+    override fun onCreateView(
+        inflater: LayoutInflater,
+        container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        binding = FragmentBookmarkBinding.inflate(layoutInflater)
+        return binding.root
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+    }
+}
