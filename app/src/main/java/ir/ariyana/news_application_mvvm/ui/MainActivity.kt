fd557b3061577c33d6a3c098b25f5a22a44341fe
Moein Latifi <moeinl1090@protonmail.com>
2022-05-03 08:44:14 +0430

    designed main activity .xml etc ...

--- app/src/main/java/ir/ariyana/news_application_mvvm/ui/MainActivity.kt
+++ app/src/main/java/ir/ariyana/news_application_mvvm/ui/MainActivity.kt
@@ -2,6 +2,8 @@ package ir.ariyana.news_application_mvvm.ui
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.MenuItem
+import ir.ariyana.news_application_mvvm.R
 import ir.ariyana.news_application_mvvm.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
@@ -12,5 +14,24 @@ class MainActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         binding = ActivityMainBinding.inflate(layoutInflater)
         setContentView(binding.root)
+
+        binding.mainActivityBottomNavigation.setOnItemSelectedListener { item ->
+
+            when(item.itemId) {
+
+                R.id.bottomMenuBreakingNews -> {
+
+                }
+
+                R.id.bottomMenuSearchNews -> {
+
+                }
+
+                R.id.bottomMenuSaveNews -> {
+
+                }
+            }
+            true
+        }
     }
 }
