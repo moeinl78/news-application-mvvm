41dcd59263a2d055754e59f5f558ecb2e99951fc
Moein Latifi <moeinl1090@protonmail.com>
2022-05-03 14:41:23 +0430

    added view model & view model factory

---
+++ app/src/main/java/ir/ariyana/news_application_mvvm/ui/ViewModelMain.kt
@@ -0,0 +1,9 @@
+package ir.ariyana.news_application_mvvm.ui
+
+import androidx.lifecycle.ViewModel
+import ir.ariyana.news_application_mvvm.repository.RepositoryMain
+
+class ViewModelMain(private val repositoryMain: RepositoryMain) : ViewModel() {
+
+
+}
---
+++ app/src/main/java/ir/ariyana/news_application_mvvm/ui/ViewModelMainFactory.kt
@@ -0,0 +1,12 @@
+package ir.ariyana.news_application_mvvm.ui
+
+import androidx.lifecycle.ViewModel
+import androidx.lifecycle.ViewModelProvider
+import ir.ariyana.news_application_mvvm.repository.RepositoryMain
+
+class ViewModelMainFactory(private val repositoryMain: RepositoryMain) : ViewModelProvider.Factory {
+
+    override fun <T : ViewModel> create(modelClass: Class<T>): T {
+        return ViewModelMain(repositoryMain) as T
+    }
+}
