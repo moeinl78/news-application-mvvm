cc58aefdd5203d8d6ad97f2ac513c49a99a99169
Moein Latifi <moeinl1090@protonmail.com>
2022-05-03 15:40:34 +0430

    modified view model main to receive data

--- app/src/main/java/ir/ariyana/news_application_mvvm/ui/ViewModelMain.kt
+++ app/src/main/java/ir/ariyana/news_application_mvvm/ui/ViewModelMain.kt
@@ -1,9 +1,43 @@
 package ir.ariyana.news_application_mvvm.ui
 
+import androidx.lifecycle.LiveData
+import androidx.lifecycle.MutableLiveData
 import androidx.lifecycle.ViewModel
+import androidx.lifecycle.viewModelScope
 import ir.ariyana.news_application_mvvm.repository.RepositoryMain
+import ir.ariyana.news_application_mvvm.repository.model.NewDataClass
+import ir.ariyana.news_application_mvvm.utils.Resource
+import kotlinx.coroutines.launch
+import retrofit2.Response
 
 class ViewModelMain(private val repositoryMain: RepositoryMain) : ViewModel() {
 
+    private val _breakingNewsData = MutableLiveData<Resource<NewDataClass>>()
+    private val _progressBar = MutableLiveData<Resource<NewDataClass>>()
 
+    val progressBar : LiveData<Resource<NewDataClass>>
+        get() = _progressBar
+
+    val breakingNewsData : LiveData<Resource<NewDataClass>>
+        get() = _breakingNewsData
+
+    var breakingNewsPage = 1
+
+    fun getBreakingNews(countryCode : String) {
+        viewModelScope.launch {
+            _progressBar.postValue(Resource.Loading())
+            val response = repositoryMain.getBreakingNews(countryCode, breakingNewsPage)
+            handleBreakingNewsResponse(response)
+        }
+    }
+
+    private fun handleBreakingNewsResponse(response : Response<NewDataClass>) : Resource<NewDataClass> {
+
+        if(response.isSuccessful) {
+            response.body()?.let { res ->
+                return Resource.Success(res)
+            }
+        }
+        return Resource.Error(response.message())
+    }
 }
