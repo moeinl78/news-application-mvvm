4c2a9a16f23c91b8e773b540075d75f3b54dadef
Moein Latifi <moeinl1090@protonmail.com>
2022-05-03 15:07:43 +0430

    create Resource file for handling api responses etc...

---
+++ app/src/main/java/ir/ariyana/news_application_mvvm/utils/Resource.kt
@@ -0,0 +1,8 @@
+package ir.ariyana.news_application_mvvm.utils
+
+sealed class Resource<T>(private val data : T ?= null, private val message : String ?= null) {
+
+    class Success<T>(data: T): Resource<T>(data)
+    class Error<T>(message: String, data: T ?= null): Resource<T>(data, message)
+    class Loading<T> : Resource<T>()
+}
